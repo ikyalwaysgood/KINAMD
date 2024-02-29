a93e9e283339d1e211f8e2aee2966996f099b0fc
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-29 19:34:23 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,51 @@
-let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
+var name = global.nameowner
+var numberowner = global.numberowner
+var gmail = global.mail
+const { 
+default: 
+makeWASocket,
+BufferJSON,
+WA_DEFAULT_EPHEMERAL, 
+generateWAMessageFromContent, 
+downloadContentFromMessage, 
+downloadHistory, 
+proto,
+getMessage, 
+generateWAMessageContent, 
+prepareWAMessageMedia 
+} = require("@adiwajshing/baileys");
+var handler = async (m, {
+conn
+}) => {
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN: ${name}
+item.ORG: Creator Bot
+item1.TEL;waid=${numberowner}:${numberowner}@s.whatsapp.net
+item1.X-ABLabel:Nomor Creator Bot 
+item2.EMAIL;type=INTERNET:${name}
+item2.X-ABLabel:Owner
+item3.ADR:;;🇮🇩 Indonesia;;;;
+item3.X-ABADR:ac
+item4.EMAIL;type=INTERNET:${name}
+item4.X-ABLabel:Developer 
+item3.ADR:;;🇮🇩 Indonesia;;;;
+item3.X-ABADR:ac 
+item5.URL:${instagram}
+item5.X-ABLabel:Website
+END:VCARD`
+const sentMsg  = await conn.sendMessage(
+    m.chat,
+    { 
+        contacts: { 
+            displayName: 'CN', 
+            contacts: [{ vcard }] 
+        }
+    }
+)
+await conn.reply(m.chat, "╳ ── ◸ *Mʏ Oᴡɴᴇʀ* ◹ ── ╳", sentMsg)}
+handler.command = handler.help = ['owner', 'creator'];
+handler.tags = ['info'];
+handler.limit = false;
+module.exports = handler;
