fe37f7b7be5c809c34fce91364e781d066608a81
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-29 19:47:26 +0700

    Update internet-ppcp.js

--- plugins/internet-ppcp.js
+++ plugins/internet-ppcp.js
@@ -1,21 +1,3 @@
-/*let fetch = require("node-fetch")
-
-let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'cewe', m)
-}
-handler.help = ['ppcouple', 'ppcp']
-handler.tags = ['internet']
-handler.command = /^(pp(cp|couple))$/i
-handler.limit = true
-
-module.exports = handler*/
-
-
 const fs = require("fs") 
 const axios = require ("axios")
 const fetch = require("node-fetch")
